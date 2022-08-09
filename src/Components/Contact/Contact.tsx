5018f16d313cdc1848f849f3c9271b95c1ff50f6
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-09 13:18:02 -0600

    "Contact" and "ContactForm" are responsive on tablet

--- src/Components/Contact/Contact.tsx
+++ src/Components/Contact/Contact.tsx
@@ -1,16 +1,47 @@
 import React from "react";
+import { useMediaQuery } from "react-responsive";
+
 import ContactForm from "../ContactForm/ContactForm";
 
 export default function Contact() {
+  const isTablet = useMediaQuery({ minWidth: 768 });
+
   return (
-    <main className="flex flex-col items-center justify-center mt-36">
-      <h1 className=" mb-2 text-40 font-livvic font-bold text-white">Contact</h1>
+    <main className="relative flex flex-col items-center justify-center mt-36 md:mt-48">
+      <h1 className=" mb-2 text-40 font-livvic font-bold text-white md:text-64">
+        Contact
+      </h1>
       <h2 className="text-32 font-livvic font-bold text-lightred">
         Ask about us
       </h2>
 
-      <div>
-        <section className="flex items-center justify-around mt-10 w-90v">
+      {isTablet && (
+        <svg
+          className="absolute -left-100px top-12"
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="200"
+        >
+          <g fill="none" fillRule="evenodd">
+            <path fill="#2C6269" d="M100 100h100V0H100z" />
+            <path
+              fill="#012F34"
+              d="M100 0C44.772 0 0 44.772 0 100s44.772 100 100 100 100-44.772 100-100S155.228 0 100 0zm0 67c18.225 0 33 14.775 33 33s-14.775 33-33 33-33-14.775-33-33 14.775-33 33-33z"
+            />
+            <path
+              fill="#79C8C7"
+              d="M100 96v4h100v-4H100zm0-8v4h100v-4H100zm0-8v4h100v-4H100zm0-8v4h100v-4H100zm0-8v4h100v-4H100zm0-8v4h100v-4H100z"
+            />
+            <path
+              fill="#F67E7E"
+              d="M196 140v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm35-8v4h4v-4h-4zm-14 0v4h4v-4h-4zm7 0v4h4v-4h-4zm-21 0v4h4v-4h-4zm7 0v4h4v-4h-4zm35 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-42 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4z"
+            />
+          </g>
+        </svg>
+      )}
+
+      <div className="md:w-65v">
+        <section className="flex items-center justify-around mt-10 w-90v md:w-auto md:justify-between">
           <svg xmlns="http://www.w3.org/2000/svg" width="65" height="72">
             <g fill="none" fillRule="evenodd" transform="translate(-7)">
               <circle cx="44" cy="44" r="28" fill="#2C6269" />
@@ -22,12 +53,12 @@ export default function Contact() {
               <path d="M0 0h56v56H0z" />
             </g>
           </svg>
-          <p className="w-70% text-lg text-white font-livvic font-bold">
+          <p className="w-70% text-lg text-white font-livvic font-bold md:w-80%">
             The quality of our talent network
           </p>
         </section>
 
-        <section className="flex items-center justify-around mt-8 w-90v">
+        <section className="flex items-center justify-around mt-8 w-90v md:w-auto md:justify-between">
           <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72">
             <g fill="none" fillRule="evenodd">
               <circle cx="44" cy="44" r="28" fill="#2C6269" />
@@ -38,12 +69,12 @@ export default function Contact() {
               />
             </g>
           </svg>
-          <p className="w-70% text-lg text-white font-livvic font-bold">
+          <p className="w-70% text-lg text-white font-livvic font-bold md:w-80%">
             Usage & implementation of our software
           </p>
         </section>
 
-        <section className="flex items-center justify-around mt-8 mb-12 w-90v">
+        <section className="flex items-center justify-around mt-8 mb-12 w-90v md:w-auto md:justify-between">
           <svg xmlns="http://www.w3.org/2000/svg" width="72" height="72">
             <g fill="none" fillRule="evenodd">
               <circle cx="44" cy="44" r="28" fill="#2C6269" />
@@ -54,13 +85,32 @@ export default function Contact() {
               />
             </g>
           </svg>
-          <p className="w-70% text-lg text-white font-livvic font-bold">
+          <p className="w-70% text-lg text-white font-livvic font-bold md:w-80%">
             How we help drive innovation
           </p>
         </section>
       </div>
 
       <ContactForm />
+
+      <svg
+        className="absolute -bottom-28 -right-28 md:bottom-0"
+        xmlns="http://www.w3.org/2000/svg"
+        width="201"
+        height="225"
+      >
+        <g fill="none" fillRule="evenodd">
+          <path
+            fill="#012F34"
+            d="M.5 100.5C.5 45.272 45.272.5 100.5.5s100 44.772 100 100-44.772 100-100 100-100-44.772-100-100zm67 0c0 18.225 14.775 33 33 33s33-14.775 33-33-14.775-33-33-33-33 14.775-33 33z"
+          />
+          <path fill="#79C8C7" d="M43.5.5v43H.5V.5z" />
+          <path
+            fill="#F67E7E"
+            d="M.5 124.986c0-55.228 44.772-100 100-100s100 44.772 100 100c0 55.229-44.772 100-100 100s-100-44.771-100-100zm67 0c0 18.226 14.775 33 33 33s33-14.774 33-33c0-18.225-14.775-33-33-33s-33 14.775-33 33z"
+          />
+        </g>
+      </svg>
     </main>
   );
 }
--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -2,34 +2,34 @@ import React from "react";
 
 export default function ContactForm() {
   return (
-    <div className="relative">
+    <div>
       <form className="relative mb-24 flex flex-col items-center" action="">
         <input
-          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white"
+          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v"
           type="text"
           placeholder="Name"
         />
 
         <input
-          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white"
+          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v"
           type="email"
           placeholder="Email Address"
         />
 
         <input
-          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white"
+          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v"
           type="text"
           placeholder="Company Name"
         />
 
         <input
-          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white"
+          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v"
           type="text"
           placeholder="Title"
         />
 
         <textarea
-          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic pt-4 pb-10 pl-4 mb-20 text-white"
+          className="w-90v bg-primary border-b-2 outline-none border-white font-livvic pt-4 pb-10 pl-4 mb-20 text-white md:w-65v"
           placeholder="Message"
         />
 
@@ -38,25 +38,6 @@ export default function ContactForm() {
           type="submit"
         />
       </form>
-
-      <svg
-        className="absolute -bottom-28 -right-32"
-        xmlns="http://www.w3.org/2000/svg"
-        width="201"
-        height="225"
-      >
-        <g fill="none" fillRule="evenodd">
-          <path
-            fill="#012F34"
-            d="M.5 100.5C.5 45.272 45.272.5 100.5.5s100 44.772 100 100-44.772 100-100 100-100-44.772-100-100zm67 0c0 18.225 14.775 33 33 33s33-14.775 33-33-14.775-33-33-33-33 14.775-33 33z"
-          />
-          <path fill="#79C8C7" d="M43.5.5v43H.5V.5z" />
-          <path
-            fill="#F67E7E"
-            d="M.5 124.986c0-55.228 44.772-100 100-100s100 44.772 100 100c0 55.229-44.772 100-100 100s-100-44.771-100-100zm67 0c0 18.226 14.775 33 33 33s33-14.774 33-33c0-18.225-14.775-33-33-33s-33 14.775-33 33z"
-          />
-        </g>
-      </svg>
     </div>
   );
 }
