68d678d41d13445245fc74d6272ab56c4a31512d
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-09 15:54:38 -0600

    "Homepage" is responsive on all devices

--- src/Components/Homepage/Homepage.tsx
+++ src/Components/Homepage/Homepage.tsx
@@ -6,34 +6,37 @@ export default function Homepage() {
 
   return (
     <div className="flex items-center justify-center bg-primary relative h-screen">
-      <svg
-        className="absolute -left-32"
-        xmlns="http://www.w3.org/2000/svg"
-        width="200"
-        height="200"
-      >
-        <g fill="none" fillRule="evenodd">
-          <path fill="#79C8C7" d="M100 200h100V100H100z" />
-          <path
-            fill="#012F34"
-            d="M100 0C44.772 0 0 44.772 0 100s44.772 100 100 100 100-44.772 100-100S155.228 0 100 0zm0 67c18.225 0 33 14.775 33 33s-14.775 33-33 33-33-14.775-33-33 14.775-33 33-33z"
-          />
-          <path
-            fill="#F67E7E"
-            d="M196 196v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm35-8v4h4v-4h-4zm-14 0v4h4v-4h-4zm7 0v4h4v-4h-4zm-21 0v4h4v-4h-4zm7 0v4h4v-4h-4zm35 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-42 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4z"
-          />
-        </g>
-      </svg>
+      
+      {isDesktop && (
+        <svg
+          className="absolute -left-32 xl:-left-100px"
+          xmlns="http://www.w3.org/2000/svg"
+          width="200"
+          height="200"
+        >
+          <g fill="none" fillRule="evenodd">
+            <path fill="#79C8C7" d="M100 200h100V100H100z" />
+            <path
+              fill="#012F34"
+              d="M100 0C44.772 0 0 44.772 0 100s44.772 100 100 100 100-44.772 100-100S155.228 0 100 0zm0 67c18.225 0 33 14.775 33 33s-14.775 33-33 33-33-14.775-33-33 14.775-33 33-33z"
+            />
+            <path
+              fill="#F67E7E"
+              d="M196 196v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm35-8v4h4v-4h-4zm-14 0v4h4v-4h-4zm7 0v4h4v-4h-4zm-21 0v4h4v-4h-4zm7 0v4h4v-4h-4zm35 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-42 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm49-8v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4zm-7 0v4h4v-4h-4z"
+            />
+          </g>
+        </svg>
+      )}
 
       <div className="flex flex-col items-center w-screen text-center mb-8 lg:flex-row lg:w-80v lg:items-start lg:gap-12 lg:h-25v">
         <div className="lg:flex lg:w-50% lg:h-full lg:items-end">
-          <h1 className="text-white text-4xl mb-5 w-55v font-livvic font-bold md:text-64 md:leading-16 md:w-50v md:mb-8 lg:text-left lg:w-full lg:mb-0 lg:text-7xl lg:leading-16">
+          <h1 className="text-white text-4xl mb-5 w-55v font-livvic font-bold md:text-64 md:leading-16 md:w-50v md:mb-8 lg:text-left lg:w-full lg:mb-0 lg:text-7xl lg:leading-16 xl:text-100 xl:leading-24">
             Find the best{" "}
             <span className="homepage__title__word text-lightred">talent</span>
           </h1>
         </div>
 
-        <div className="flex flex-col items-center justify-center lg:w-50% lg:h-full lg:justify-between lg:items-start">
+        <div className="flex flex-col items-center justify-center lg:w-50% lg:h-full lg:justify-between lg:items-start xl:pl-32">
           {isDesktop && (
             <svg
               width="50"
