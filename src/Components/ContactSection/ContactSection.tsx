6ec12bf2aa4580bd92468d3056b4c10fa8ffe35c
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-09 17:03:18 -0600

    "ContactSection" is responsive on all devices

--- src/Components/ContactSection/ContactSection.tsx
+++ src/Components/ContactSection/ContactSection.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function ContactSection() {
   return (
     <div className="contact-section relative flex flex-col items-center justify-center bg-lightred md:flex-row md:py-24 md:gap-8">
-      <h1 className="contact-section__title w-70v mt-24 mb-4 text-center text-32 leading-8 font-livvic font-bold text-darkergreen md:mt-0 md:mb-0 md:w-50%">
+      <h1 className="contact-section__title w-70v mt-24 mb-4 text-center text-32 leading-8 font-livvic font-bold text-darkergreen md:mt-0 md:mb-0 md:w-50% lg:text-5xl">
         Ready to get started?
       </h1>
       <button className="contact-section__button mb-24 border-darkergreen border-2 rounded-3xl py-1.5 px-8 text-lg text-darkergreen font-livvic font-bold md:mb-0">
         contact us
       </button>
       <svg
-        className="contact__patter absolute -bottom-5 left-0"
+        className="contact__patter absolute -bottom-5 left-0 lg:bottom-0"
         xmlns="http://www.w3.org/2000/svg"
         width="200"
         height="144"
