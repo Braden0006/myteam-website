e19a7e44328030f84341540b8ce07bb18a9e8849
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-11 19:02:25 -0600

    Added some hover styling is "ContactForm"

--- src/Components/ContactForm/ContactForm.tsx
+++ src/Components/ContactForm/ContactForm.tsx
@@ -5,36 +5,37 @@ export default function ContactForm() {
     <div className="lg:h-90%">
       <form className="relative mb-24 flex flex-col items-center" action="">
         <input
-          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full"
+          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full lg:hover:cursor-pointer lg:hover:border-cyan"
           type="text"
           placeholder="Name"
         />
 
         <input
-          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full"
+          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full lg:hover:cursor-pointer lg:hover:border-cyan"
           type="email"
           placeholder="Email Address"
+          required
         />
 
         <input
-          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full"
+          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full lg:hover:cursor-pointer lg:hover:border-cyan"
           type="text"
           placeholder="Company Name"
         />
 
         <input
-          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full"
+          className="w-90v bg-primary border-b outline-none border-white font-livvic py-4 pl-4 mb-8 text-white md:w-65v lg:w-full lg:hover:cursor-pointer lg:hover:border-cyan"
           type="text"
           placeholder="Title"
         />
 
         <textarea
-          className="w-90v bg-primary border-b outline-none border-white font-livvic pt-4 pb-10 pl-4 mb-20 text-white md:w-65v lg:w-full"
+          className="w-90v bg-primary border-b outline-none border-white font-livvic pt-4 pb-10 pl-4 mb-20 text-white md:w-65v lg:w-full lg:hover:cursor-pointer lg:hover:border-cyan"
           placeholder="Message"
         />
 
         <input
-          className="absolute bottom-0 left-0 py-3 px-8 rounded-full bg-white text-primary font-livvic font-bold"
+          className="absolute bottom-0 left-0 py-3 px-8 rounded-full bg-white text-primary font-livvic font-bold lg:hover:cursor-pointer lg:hover:border-cyan"
           type="submit"
         />
       </form>
