59a766b646e50d886358ad526698a4fc4195ba48
Braden0006 <96596757+Braden0006@users.noreply.github.com>
2022-08-11 20:09:43 -0600

    Added styling when hovered in "AboutDirectorsCards"

--- src/Components/AboutDirectorsCards/AboutDirectorsCards.tsx
+++ src/Components/AboutDirectorsCards/AboutDirectorsCards.tsx
@@ -42,7 +42,7 @@ export default function AboutDirectorsCards() {
 
         {nikita ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setNikita(false)}
           >
             <svg
@@ -62,7 +62,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setNikita(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -88,12 +88,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
@@ -117,7 +119,7 @@ export default function AboutDirectorsCards() {
         </span>
         {christian ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setChristian(false)}
           >
             <svg
@@ -137,7 +139,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setChristian(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -163,12 +165,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
@@ -190,7 +194,7 @@ export default function AboutDirectorsCards() {
         </span>
         {cruz ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setCruz(false)}
           >
             <svg
@@ -210,7 +214,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setCruz(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -234,12 +238,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
@@ -264,7 +270,7 @@ export default function AboutDirectorsCards() {
 
         {drake ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setDrake(false)}
           >
             <svg
@@ -284,7 +290,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setDrake(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -310,12 +316,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
@@ -340,7 +348,7 @@ export default function AboutDirectorsCards() {
 
         {griffin ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setGriffin(false)}
           >
             <svg
@@ -360,7 +368,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setGriffin(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -386,12 +394,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
@@ -414,7 +424,7 @@ export default function AboutDirectorsCards() {
 
         {aden ? (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-cyan rounded-full z-10 lg:hover:cursor-pointer"
             onClick={() => setAden(false)}
           >
             <svg
@@ -434,7 +444,7 @@ export default function AboutDirectorsCards() {
           </span>
         ) : (
           <span
-            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10"
+            className="absolute -bottom-7 flex justify-center items-center h-14 w-14 bg-lightred rounded-full z-10 lg:hover:bg-cyan lg:hover:cursor-pointer"
             onClick={() => setAden(true)}
           >
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16">
@@ -458,12 +468,14 @@ export default function AboutDirectorsCards() {
             <div className="flex gap-4">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
                 />
               </svg>
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20">
                 <path
+                  className="lg:hover:fill-lightred lg:hover:cursor-pointer"
                   fill="#FFF"
                   d="M18 0H2C.9 0 0 .9 0 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM6 17H3V8h3v9zM4.5 6.3c-1 0-1.8-.8-1.8-1.8s.8-1.8 1.8-1.8 1.8.8 1.8 1.8-.8 1.8-1.8 1.8zM17 17h-3v-5.3c0-.8-.7-1.5-1.5-1.5s-1.5.7-1.5 1.5V17H8V8h3v1.2c.5-.8 1.6-1.4 2.5-1.4 1.9 0 3.5 1.6 3.5 3.5V17z"
                 />
